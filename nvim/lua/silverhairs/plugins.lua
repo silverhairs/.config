0a126788a337620530cda012e8e1664f0c3cf70b
Boris Kayi <hello@silverhairs.engineer>
2023-06-08 00:43:57 +0200

    feat: add nvim-tree.lua

--- nvim/lua/silverhairs/plugins.lua
+++ nvim/lua/silverhairs/plugins.lua
@@ -16,25 +16,34 @@ return require('packer').startup(function(use)
         'lewis6991/gitsigns.nvim',
         -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
     }
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
 }
 
 end)
--- nvim/lua/silverhairs/remap.lua
+++ nvim/lua/silverhairs/remap.lua
@@ -1,16 +1,18 @@
+local keymap = vim.keymap
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>E", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+keymap.set("n", "<leader>E", vim.cmd.Ex)
+keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 -- Allows you to move selected lines up and down with `W` and `S` keys cause I hate the whole `J` and `K` thing
-vim.keymap.set("v", "S", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "W", ":m '<-2<CR>gv=gv")
+keymap.set("v", "S", ":m '>+1<CR>gv=gv")
+keymap.set("v", "W", ":m '<-2<CR>gv=gv")
 
 -- Push yanked data to system's clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+keymap.set("n", "<leader>y", "\"+y")
+keymap.set("v", "<leader>y", "\"+y")
+keymap.set("n", "<leader>Y", "\"+Y")
 
 -- Replace all occurences of the word where the cursor is located
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
