2b6b972d2db179797058bb4811814bf1c36a8549
Boris Kayi <hello@silverhairs.engineer>
2023-08-02 22:24:50 +0200

    remap: change lsp confirmation to `<CR>`

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,7 +15,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({confirm = true}),
+	['<CR>'] = cmp.mapping.confirm({confirm = true}),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 
