bb3e4c238496d92bd3e07ea25e7daf53c006335d
Boris Kayi <hello@silverhairs.engineer>
2024-10-25 18:31:42 +0200

    chore: update helix configs for kotlin

---
+++ .dart-cli-completion/zsh-config.zsh
@@ -0,0 +1,6 @@
+
+## [fvm]
+## Completion config for "fvm"
+[[ -f /Users/silverhairs/.config/.dart-cli-completion/fvm.zsh ]] && . /Users/silverhairs/.config/.dart-cli-completion/fvm.zsh || true
+## [/fvm]
+
