832c040c779d008baae52057d8f3b9451a98841e
Boris Kayi <hello@silverhairs.engineer>
2024-11-09 09:27:47 +0100

    chore: many changes

--- .dart-cli-completion/zsh-config.zsh
+++ .dart-cli-completion/zsh-config.zsh
@@ -1,6 +1,12 @@
 
 ## [fvm]
 ## Completion config for "fvm"
-[[ -f /Users/silverhairs/.config/.dart-cli-completion/fvm.zsh ]] && . /Users/silverhairs/.config/.dart-cli-completion/fvm.zsh || true
+[[ -f /Users/$USER/.config/.dart-cli-completion/fvm.zsh ]] && . /Users/silverhairs/.config/.dart-cli-completion/fvm.zsh || true
 ## [/fvm]
+
+
+## [mason]
+## Completion config for "mason"
+[[ -f /Users/$USER/.config/.dart-cli-completion/mason.zsh ]] && . /Users/$USER/.config/.dart-cli-completion/mason.zsh || true
+## [/mason]
 
